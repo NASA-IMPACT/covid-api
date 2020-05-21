7d6e15c73b3dcd73cc3d0ff07edea102d3bde794
Olaf Veerman <olaf.veerman@gmail.com>
2020-05-21 14:03:57 -0400

    Add docs

--- covid_api/models/common.py
+++ covid_api/models/common.py
@@ -2,6 +2,13 @@ from typing import List, Tuple, Dict
 from pydantic import BaseModel
 
 class Polygon(BaseModel):
+    """
+    Polygon structured as a geometry object of a GeoJSON feature.
+
+    https://tools.ietf.org/html/rfc7946#section-3.1.6
+
+    """
+
     type: str = "Polygon"
     coordinates: List[List[Tuple[float, float]]]
 
