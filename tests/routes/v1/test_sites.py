c668653c1866660c935d46b369a97e5b2069b73f
Drew Bollinger <drew@developmentseed.org>
2020-05-28 00:13:26 -0400

    Try aws creds file

--- tests/routes/v1/test_sites.py
+++ tests/routes/v1/test_sites.py
@@ -10,23 +10,12 @@ def test_sites(app):
     response = app.get("/v1/sites")
     assert response.status_code == 200
 
-@pytest.fixture(scope='function')
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-    os.environ['AWS_SESSION_TOKEN'] = 'testing'
-
-@pytest.fixture(scope='function')
-def s3(aws_credentials):
-    with mock_s3():
-        yield boto3.client('s3', region_name='us-east-1')
-
+@mock_s3
 def test_site_id(app, aws_credentials, s3):
     """test /sites/{id} endpoint"""
 
     # aws mocked resources
+    s3 = boto3.client('s3')
     s3.create_bucket(Bucket=INDICATOR_BUCKET)
     s3.put_object(Bucket=INDICATOR_BUCKET, Key='indicators/test/super.csv', Body=b'test')
 
