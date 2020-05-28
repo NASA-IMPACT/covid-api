f8e38393a92b611713433fde05c40ba4914c8a22
Drew Bollinger <drew@developmentseed.org>
2020-05-27 23:54:26 -0400

    Try pytest fixture

--- tests/routes/v1/test_sites.py
+++ tests/routes/v1/test_sites.py
@@ -1,6 +1,7 @@
 """Test /v1/sites endpoints"""
 import os
 import boto3
+import pytest
 from moto import mock_s3
 from covid_api.core.config import INDICATOR_BUCKET
 
@@ -9,14 +10,19 @@ def test_sites(app):
     response = app.get("/v1/sites")
     assert response.status_code == 200
 
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+
 @mock_s3
-def test_site_id(app):
+def test_site_id(app, aws_credentials):
     """test /sites/{id} endpoint"""
 
     # aws mocking
-    os.environ["AWS_ACCESS_KEY_ID"] = "test"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
-
     s3 = boto3.client('s3')
     s3.create_bucket(Bucket=INDICATOR_BUCKET)
     s3.put_object(Bucket=INDICATOR_BUCKET, Key='indicators/test/super.csv', Body=b'test')
