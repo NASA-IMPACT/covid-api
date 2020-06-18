0d8a881cca62e41f210784d8df57884c0cbd55b4
Drew Bollinger <drew@developmentseed.org>
2020-06-18 13:30:03 -0400

    Update tests

--- tests/routes/v1/test_sites.py
+++ tests/routes/v1/test_sites.py
@@ -5,8 +5,17 @@ from moto import mock_s3
 from covid_api.core.config import INDICATOR_BUCKET
 
 
+@mock_s3
 def test_sites(app):
     """test /sites endpoint"""
+
+    # aws mocked resources
+    s3 = boto3.client("s3")
+    s3.create_bucket(Bucket=INDICATOR_BUCKET)
+    s3.put_object(
+        Bucket=INDICATOR_BUCKET, Key="indicators/test/super.csv", Body=b"test"
+    )
+
     response = app.get("/v1/sites")
     assert response.status_code == 200
 
