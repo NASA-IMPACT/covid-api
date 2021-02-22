d2df2757444394ab349af411466a7bda1f0d9d14
Leo Thomas <leo.thomas@mail.mcgill.ca>
2021-02-22 12:50:01 -0500

    Added `euports` into dataset source tile for `nightlights-viirs dataset, for spotlights `du` and `gh`

--- covid_api/db/static/datasets/__init__.py
+++ covid_api/db/static/datasets/__init__.py
@@ -169,7 +169,7 @@ class DatasetManager(object):
             # spotlight id (if a spotlight was requested)
             format_url_params = dict(api_url=api_url)
             if spotlight_id:
-                if k == "nightlights-hd" and spotlight_id in ["du", "gh"]:
+                if k == "nightlights-viirs" and spotlight_id in ["du", "gh"]:
                     spotlight_id = "EUPorts"
                 format_url_params.update(dict(spotlight_id=spotlight_id))
 
