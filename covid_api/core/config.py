616a9aff17b4785966d52bc1c8f21800d346177b
Leo Thomas <leo.thomas@mail.mcgill.ca>
2021-02-22 11:26:23 -0500

    renamed suez canal id from sz to sc

--- covid_api/core/config.py
+++ covid_api/core/config.py
@@ -21,11 +21,11 @@ MEMCACHE_PASSWORD = os.environ.get("MEMCACHE_PASSWORD")
 INDICATOR_BUCKET = os.environ.get("INDICATOR_BUCKET", "covid-eo-data")
 
 DATASET_METADATA_FILENAME = os.environ.get(
-    "DATASET_METADATA_FILENAME", "dev-dataset-metadata.json"
+    "DATASET_METADATA_FILENAME", "staging-dataset-metadata.json"
 )
 
 DATASET_METADATA_GENERATOR_FUNCTION_NAME = os.environ.get(
-    "DATASET_METADATA_GENERATOR_FUNCTION_NAME", "dev-dataset-metadata-generator"
+    "DATASET_METADATA_GENERATOR_FUNCTION_NAME", "staging-dataset-metadata-generator"
 )
 
 DT_FORMAT = "%Y-%m-%d"
