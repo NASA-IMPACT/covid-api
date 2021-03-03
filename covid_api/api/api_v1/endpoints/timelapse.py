7fc2ff2c66ea709f822e6c3af6f24cb35e6b56fe
Aimee Barciauskas <aimee@developmentseed.org>
2021-03-03 15:48:57 -0800

    Switch default timelapse endpoint

--- covid_api/api/api_v1/endpoints/timelapse.py
+++ covid_api/api/api_v1/endpoints/timelapse.py
@@ -15,8 +15,8 @@ router = APIRouter()
 )
 def timelapse(query: TimelapseRequest):
     """Handle /timelapse requests."""
-    if query.type == "no2":
-        url = f"s3://covid-eo-data/OMNO2d_HRM/OMI_trno2_0.10x0.10_{query.month}_Col3_V4.nc.tif"
+    if query.type == "modis-vi":
+        url = f"s3://modis-vi-nasa/MOD13A1.006/2018.01.01.tif"
     else:
         url = f"s3://covid-eo-data/xco2-mean/xco2_16day_mean.{query.month}.tif"
     mean, median = get_zonal_stat(query.geojson, url)
