ad839c6ec500541399ce0e38033cbb4aa21931b6
Aimee Barciauskas <aimee@developmentseed.org>
2021-03-13 13:51:04 -0800

    Revert change to timelapse

--- covid_api/api/api_v1/endpoints/timelapse.py
+++ covid_api/api/api_v1/endpoints/timelapse.py
@@ -1,7 +1,5 @@
 """API metadata."""
 
-import datetime
-
 from covid_api.api.utils import get_zonal_stat
 from covid_api.models.timelapse import TimelapseRequest, TimelapseValue
 
@@ -17,19 +15,8 @@ router = APIRouter()
 )
 def timelapse(query: TimelapseRequest):
     """Handle /timelapse requests."""
-    landsat_stac_api = 'https://eod-catalog-svc-prod.astraea.earth/search?collection=landsat8_l1tp'
-    # translate query date time to compliant date format
-    # create bounding box from request or post polygon
-    print(query.geojson.geometry.coordinates)
-    coordinates = query.geojson.geometry.coordinates[0]
-    bbox_coordinates = map(str, [coordinates[0][0], coordinates[0][1], coordinates[1][0], coordinates[2][1]])
-    bbox = ','.join(bbox_coordinates)
-    datetime_string = query.month
-    datetime_obj = datetime.datetime.strptime(datetime_string, '%Y.%m.%d')
-    landsat_stac_api_search = f"{landsat_stac_api}&bbox={bbox}&datetime={datetime_obj.isoformat()}Z"
-    print(landsat_stac_api_search)
-    if query.type == "modis-vi":
-        url = f"s3://modis-vi-nasa/MOD13A1.006/{query.month}.tif"
+    if query.type == "no2":
+        url = f"s3://covid-eo-data/OMNO2d_HRM/OMI_trno2_0.10x0.10_{query.month}_Col3_V4.nc.tif"
     else:
         url = f"s3://covid-eo-data/xco2-mean/xco2_16day_mean.{query.month}.tif"
     mean, median = get_zonal_stat(query.geojson, url)
