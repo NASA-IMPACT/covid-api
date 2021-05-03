f0afb9b3731b5dbc5f49f79886da79f38f09c2b9
Aimee Barciauskas <aimee@developmentseed.org>
2021-05-03 09:11:55 -0700

    Fix modis data endpoint

--- covid_api/api/api_v1/endpoints/timelapse.py
+++ covid_api/api/api_v1/endpoints/timelapse.py
@@ -15,9 +15,6 @@ router = APIRouter()
 )
 def timelapse(query: TimelapseRequest):
     """Handle /timelapse requests."""
-    if query.type == "no2":
-        url = f"s3://covid-eo-data/OMNO2d_HRM/OMI_trno2_0.10x0.10_{query.month}_Col3_V4.nc.tif"
-    else:
-        url = f"s3://covid-eo-data/xco2-mean/xco2_16day_mean.{query.month}.tif"
+    url = f"https://modis-vi-nasa.s3.amazonaws.com/MOD13A1.006/{query.month}.tif"
     mean, median = get_zonal_stat(query.geojson, url)
     return dict(mean=mean, median=median)
