0458aaaf4b4e7cfd64967ec22fd02049bbb17412
Leo Thomas <leothomas@users.noreply.github.com>
2021-06-10 10:25:33 -0400

    Revert "Feature: abstract timelapse endpoint"

--- covid_api/models/timelapse.py
+++ covid_api/models/timelapse.py
@@ -1,15 +1,8 @@
 """Tilelapse models."""
-import re
-from typing import List, Optional
 
 from geojson_pydantic.features import Feature
 from geojson_pydantic.geometries import Polygon
-from pydantic import BaseModel, validator
-
-
-def to_camel(s):
-    """Convert string s from `snake_case` to `camelCase`"""
-    return re.sub(r"(?!^)_([a-zA-Z])", lambda m: m.group(1).upper(), s)
+from pydantic import BaseModel
 
 
 class PolygonFeature(Feature):
@@ -19,35 +12,15 @@ class PolygonFeature(Feature):
 
 
 class TimelapseValue(BaseModel):
-    """ "Timelapse values model."""
+    """"Timelapse values model."""
 
-    mean: Optional[float]
-    median: Optional[float]
-    date: Optional[str]
-    error: Optional[str]
+    mean: float
+    median: float
 
 
 class TimelapseRequest(BaseModel):
-    """ "Timelapse request model."""
+    """"Timelapse request model."""
 
-    # TODO: parse this into a python `datetime` object (maybe using a validator? )
-    # TODO: validate that exactly one of `date` or `date_range` is supplied
-    date: Optional[str]
-    date_range: Optional[List[str]]
+    month: str
     geojson: PolygonFeature
-    dataset_id: str
-    spotlight_id: Optional[str]
-
-    @validator("date_range")
-    def validate_date_objects(cls, v):
-
-        """Validator"""
-        if not len(v) == 2:
-            raise ValueError("Field `dateRange` must contain exactly 2 dates")
-        return v
-
-    class Config:
-        """Generate alias to convert `camelCase` requests to `snake_case` fields to be used
-        within the code"""
-
-        alias_generator = to_camel
+    type: str
